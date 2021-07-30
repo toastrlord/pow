ea887060458ec8fa955bb34d3cfc81482e6156da
Rob Morandini <robjmorandini@hotmail.com>
2021-07-30 19:50:36 -0400

    Add basic username functionality

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,45 @@ const lobby = require('../lobby');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('lobby', { title: 'Express', messages: lobby.getMessages()});
-});
-
-router.post('/', function(req, res, next) {
-  console.log('POSTAN');
-  body('message').trim().escape(),
-  (req, res, next) => {
-    lobby.addMessage('Me', req.body.message);
-    console.log(lobby.getMessages());
-    res.redirect('/', {title: 'Express', messages: lobby.getMessages()})
+  if (req.cookies.name) {
+    res.render('lobby', {messages: lobby.getMessages()});
+  }
+  else {
+    res.redirect('/signin');
   }
 });
+
+router.post('/',
+  body('message').trim().isLength({min: 1}).withMessage('Please enter a message!'),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render('lobby', {messages: lobby.getMessages(), errors: errors.array()});
+    }
+    else {
+      lobby.addMessage(req.cookies.name, req.body.message);
+      res.redirect('/');
+    }
+  }
+);
+
+
+router.get('/signin', function(req, res, next) {
+  res.render('signin');
+});
+
+router.post('/signin',
+  body('name').trim().isLength({min: 1}).withMessage('Please enter a name.'),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render('signin', {errors: errors.array()});
+    }
+    else {
+      res.cookie('name', req.body.name, {maxAge: 36000});
+      res.redirect('/');
+    }
+  }
+);
 
 module.exports = router;
