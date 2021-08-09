16c3007917e1ba5729b91466b73ad7d4c4617d4c
Rob Morandini <robjmorandini@hotmail.com>
2021-08-08 21:27:45 -0400

    Start work on .vue file for game screen

---
+++ src/components/Game.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <p v-for='player in players' :key='player.name'>
+            {{ player.name }}
+        </p>
+        <card v-for='card in cards' :key='card.name' v-bind:card='card'/>
+    </div>
+</template>
+
+<script>
+import Card from './Card.vue';
+import socket from '../socket';
+
+    export default {
+        name: 'Game',
+        components: [
+            Card,
+        ],
+        data() {
+            return {
+                cards: [],
+            };
+        },
+        created() {
+            socket.on('draw cards', newCards => {
+                cards.push(newCards);
+            });
+        },
+    }
+</script>
